8d5e191428472fc1af4fff043eb8827b11e8b988
JackBarrett <jackbhd96@gmail.com>
2019-10-21 22:53:28 +0100

    Added a Gravity variable which affects the player model if no object is below them. Rearrange functions to better suit the scripts purpose.

--- Assets/Scripts/VrController.cs
+++ Assets/Scripts/VrController.cs
@@ -5,7 +5,7 @@ using Valve.VR;
 
 public class VrController : MonoBehaviour
 {
-
+    public float Gravity = 30.0f;
     public float Sensitivity = 0.1f;
     public float MaxSpeed = 1.0f;
 
@@ -15,7 +15,6 @@ public class VrController : MonoBehaviour
     private float _speed = 0.0f;
 
     private CharacterController _characterController = null;
-    private Transform _cameraRig = null;
     private Transform _head = null;
 
     private void Awake()
@@ -25,61 +24,16 @@ public class VrController : MonoBehaviour
 
     private void Start()
     {
-        _cameraRig = SteamVR_Render.Top().origin;
         _head = SteamVR_Render.Top().head;
     }
-
+    
     private void Update()
     {
-        HandleHead();
         HandleHeight();
         CalculateMovement();
     }
 
-    private void HandleHead()
-    {
-        //Store current
-        Vector3 oldPosition = _cameraRig.position;
-        Quaternion oldRotation = _cameraRig.rotation;
-
-        //Rotation
-        transform.eulerAngles = new Vector3(0.0f, _head.rotation.eulerAngles.y, 0.0f);
-
-        //Restore position
-
-        _cameraRig.position = oldPosition;
-        _cameraRig.rotation = oldRotation;
-    }
-
-    private void CalculateMovement()
-    {
-        //Calculate movement orientation
-
-        Vector3 orientationEuler = new Vector3(0, transform.eulerAngles.y, 0);
-        Vector3 movement = Vector3.zero;
-        Quaternion orientation = Quaternion.Euler(orientationEuler);
-
-        //If not moving set speed to zero
-        if (TouchPadPressed.GetLastStateUp(SteamVR_Input_Sources.Any))
-            _speed = 0;
-
-        //If button pressed on either controller
-        if (TouchPadPressed.state)
-        {
-            // Add clamp to speed
-            _speed += TouchPadValue.axis.y * Sensitivity;
-            _speed = Mathf.Clamp(_speed, -MaxSpeed, MaxSpeed);
-
-            //Orientation
-            movement += orientation * (_speed * Vector3.forward * Time.deltaTime);
-        }
-
-
-        //Apply
-
-        _characterController.Move(movement);
-    }
-
+    //Changes CharacterController height to be the cameras height
     private void HandleHeight()
     {
         //Get head of player in local space
@@ -103,4 +57,38 @@ public class VrController : MonoBehaviour
         //Apply
         _characterController.center = newCenter;
     }
+
+    private void CalculateMovement()
+    {
+        //Calculate movement orientation
+
+        Vector3 orientationEuler = new Vector3(0, _head.eulerAngles.y, 0);
+        Vector3 movement = Vector3.zero;
+        Quaternion orientation = Quaternion.Euler(orientationEuler);
+
+        //If not moving set speed to zero
+        if (TouchPadPressed.GetLastStateUp(SteamVR_Input_Sources.Any))
+            _speed = 0;
+
+        //If button pressed on either controller
+        if (TouchPadPressed.state)
+        {
+            // Add clamp to speed
+            _speed += TouchPadValue.axis.y * Sensitivity;
+            _speed = Mathf.Clamp(_speed, -MaxSpeed, MaxSpeed);
+
+            //Orientation
+            movement += orientation * (_speed * Vector3.forward);
+        }
+
+        //Gravity
+        movement.y -= Gravity * Time.deltaTime;
+
+
+        //Apply
+
+        _characterController.Move(movement * Time.deltaTime);
+    }
+
+    
 }
